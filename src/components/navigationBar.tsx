0731d3e3803d5d6566a7682412bbfff650fda0e2
ankanroy-code <8ankanroy@gmail.com>
2022-07-21 19:54:35 +0530

    re-done everything
    Signed-off-by: ankanroy-code <8ankanroy@gmail.com>

--- src/components/navigationBar.tsx
+++ src/components/navigationBar.tsx
@@ -1,46 +1,36 @@
-import { useEffect, useState } from 'react';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { BiCart, BiUserCircle } from 'react-icons/bi';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-import SearchBar from './searchBar';
-import SideBar from './sideBar';
+import SearchBar from "./searchBar";
 
 // smallScreen default FIRST APPROCH
 
 export default function NavigationBar(props: {
   breakPoint: number;
+  title: string;
 }): JSX.Element {
-  const [screenWidth, setScreenWidth] = useState<number>(0);
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setScreenWidth(window.innerWidth);
-    })
-  }, []);
-  if (screenWidth > props.breakPoint) {
-    return <div className="flex flex-row w-screen gap-3">
-      <p className="text-4xl flex flex-row "><GiHamburgerMenu /></p>
-      <p className="basis-1 text-4xl">BABAG.com</p>
-      <div className="basis-9/12 flex flex-col w-full"><SearchBar /></div>
-      <div className="basis-1 text-4xl flex flex-row w-full gap-2  justify-end">
-        <BiCart />
-        <BiUserCircle />
-      </div>
-    </div>;
-  } else {
-    return (
-      <div className="flex flex-col w-screen">
-        <div className="flex flex-row gap-2 w-full">
-          <button className="text-4xl flex flex-row "><GiHamburgerMenu /></button>
-          <h1 className="text-4xl">BABAG.com</h1>
-          <div className="text-4xl flex flex-row w-full gap-2  justify-end">
-            <p><BiUserCircle /></p>
-            <p><AiOutlineShoppingCart /></p>
-          </div>
+  return (
+    <nav className="text-3xl lg:text-4xl w-screen flex flex-col m-1">
+      <div className="w-full flex justify-between">
+        <div className="flex flex-row justify-start">
+          <GiHamburgerMenu />
         </div>
-        <SearchBar />
-        <SideBar breakPoint={700} isVisible={true} />
+        <div className="flex flex-row justify-center">
+          <h1>{props.title}</h1>
+        </div>
+        <div className="hidden md:block flex flex-row justify-center">
+          <SearchBar />
+        </div>
+        <div className="flex flex-row m-1 justify-end">
+          <AiOutlineShoppingCart />
+          <BiUser />
+        </div>
       </div>
-    );
-  }
+
+      <div className="block md:hidden flex flex-row w-full">
+        <SearchBar />
+      </div>
+    </nav>
+  );
 }
--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -2,9 +2,9 @@ import {AiOutlineSearch} from 'react-icons/ai';
 
 export default function SearchBar() {
   return (
-    <div className="flex flex-row">
-      <input className="basis-11/12 text-2xl outline outline-1"></input>
-      <p className="basis-1 text-4xl flex flex-row w-full justify-end"><AiOutlineSearch /></p>
+    <div className="flex flex-row w-full">
+      <input className="outline outline-1 w-full"></input>
+      <AiOutlineSearch className="flex justify-center w-10 m-1"/>
     </div>
   );
 }
