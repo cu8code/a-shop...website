9f0591290a612c35c8457c8e57cee9b8cc50eca4
cu8code <git8ankanroy@gmail.com>
2022-07-24 00:28:03 +0530

    added some more stuff

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -7,8 +7,10 @@ export default function SideBar({
   isVisible: boolean,
   changeIsVisible: Dispatch<SetStateAction<boolean>>
   }): JSX.Element {
+    console.log(isVisible);
+    
   return (
-    <div className={`transform ${isVisible ? "-translate-x-full" : ""} absolute top-0 left-0 flex flex-col w-3/5 h-screen m-0 p-0 bg-slate-900`}>
+    <div className={`transform ${!isVisible ? "-translate-x-full" : ""} absolute top-0 left-0 flex flex-col w-3/5 h-screen m-0 p-0 bg-slate-900`}>
       <button onClick={()=> isVisible ? changeIsVisible(false) : changeIsVisible(true)} className="text-pink-50">CLOSE</button>
     </div>
   );
